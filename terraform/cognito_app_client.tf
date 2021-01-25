61e1ead3e697145d8061d73b1b14698580c0718b
Pramod Jangam <pramod.jangam@deserve.com>
2021-01-25 11:11:47 +0530

    corrected tf script

--- terraform/cognito_app_client.tf
+++ terraform/cognito_app_client.tf
@@ -2,7 +2,7 @@
 resource "aws_cognito_user_pool_client" "client" {
   name = "client"
 
-  user_pool_id = data.aws_cognito_user_pools.selected.value
+  user_pool_id = data.aws_cognito_user_pools.selected.id
 }
 
 
